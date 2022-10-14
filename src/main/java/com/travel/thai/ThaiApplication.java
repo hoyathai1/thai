1463caf11e3a9b1ae6e0e4d179834b5bbf6fd480
rlagusgh3421 <haha9006@gmail.com>
2022-10-15 03:47:04 +0900

    Initial commit

---
+++ src/main/java/com/travel/thai/ThaiApplication.java
@@ -0,0 +1,13 @@
+package com.travel.thai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/travel/thai/ThaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.thai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
